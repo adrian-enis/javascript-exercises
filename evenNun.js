192ee3d041f03bf17882751f7bd132f8b6df2e92
adrian enis <adrianenis2000@gmail.com>
2026-09-07 13:07:45 -0300

    commit

--- evenNun.js
+++ evenNun.js
@@ -3,20 +3,34 @@ Dado un par de numeros, encontrar cuantos impares hay entre ellos
 ejem: (1,100) = 50
 * */
 
-function evenNum(num1,num2){
+// function evenNum(num1,num2){
 
-    let inicio = Math.min(num1,num2);
-    let final = Math.max(num1,num2);
-    let total = final - inicio + 1;
-    let resultado;
+//     let inicio = Math.min(num1,num2);
+//     let final = Math.max(num1,num2);
+//     let total = final - inicio + 1;
+//     let resultado;
 
-    if(inicio % 2 !== 0 && final % 2 !== 0){
-        resultado = (total / 2) + 1;
-    }else{
-        resultado = (total / 2);
-    }
-    return resultado
-}
+//     if(inicio % 2 !== 0 && final % 2 !== 0){ 
+//         resultado = Math.floor(total / 2) + 1;
+//     }else{
+//         resultado = Math.floor(total / 2);
+//     }
+//     return resultado
+// }
 
-console.log(evenNum(1,3866))
+// console.log(evenNum(1,3866))
 //Mañana hacer el otro metodo
+
+function evenNum(num1, num2){
+    let inicio = Math.min(num1,num2)
+    let final = Math.max(num1,num2)
+    let contador = 0;
+    for(let i = inicio; i <= final; i++){
+        if(i % 2 !== 0){
+            contador++
+        }
+    }
+
+    return contador
+}
+console.log(evenNum(1,3866))
